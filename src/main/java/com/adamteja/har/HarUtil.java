9eadd867f8883c51a1effc3ef8f424f9a2df4752
ateja <ateja@wsgc.com>
2019-09-17 16:43:14 -0400

    updated dependancy versions, added executable jar to pom

--- src/main/java/com/adamteja/har/HarUtil.java
+++ src/main/java/com/adamteja/har/HarUtil.java
@@ -1,18 +1,18 @@
 package com.adamteja.har;
 
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import de.sstoehr.harreader.HarReader;
 import de.sstoehr.harreader.HarReaderException;
 import de.sstoehr.harreader.model.Har;
 import de.sstoehr.harreader.model.HarEntry;
 import de.sstoehr.harreader.model.HarHeader;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.net.URLDecoder;
+import java.util.*;
 
 public class HarUtil {
 
@@ -34,10 +34,19 @@ public class HarUtil {
         sourceEntries = applyPrimaryDomainFilter(sourceEntries, primaryDomain);
         sourceEntries = applyBlacklist(sourceEntries);
 
+        System.out.println("Source Entries: "+sourceEntries.size());
+        writeHARToJSONFile(sourceEntries, "source.json", primaryDomain);
+
         targetEntries = targetHar.getLog().getEntries();
         primaryDomain = targetHar.getLog().getPages().get(0).getTitle();
         targetEntries = applyPrimaryDomainFilter(targetEntries, primaryDomain);
         targetEntries = applyBlacklist(targetEntries);
+
+
+        System.out.println("Target Entries: "+targetEntries.size());
+        writeHARToJSONFile(targetEntries, "target.json", primaryDomain);
+
+
 
     }
 
@@ -291,7 +300,7 @@ public class HarUtil {
         List<String[]> targetUniquesDiff = new ArrayList<>();
 
 
-        System.out.println("===================");
+        System.out.println("diffUniquesLists===================");
         System.out.println("Initial sizes Source: "+ sourceUniques.size() +" Target: "+targetUniques.size());
 
         //find source uniques
@@ -323,6 +332,8 @@ public class HarUtil {
         }
         //source output
         System.out.println("==== Source ("+ sourceUniquesDiff.size() +")====");
+        System.out.println("URL   | MIMETYPE  | STATUS  | COUNT | ");
+
         for(int i = 0; i < sourceUniquesDiff.size(); i++) {
             String sUrl = sourceUniquesDiff.get(i)[0];
             String sMimeType = sourceUniquesDiff.get(i)[1];
@@ -361,6 +372,8 @@ public class HarUtil {
         }
         //target output
         System.out.println("==== Target ("+ targetUniquesDiff.size() +")====");
+        System.out.println("URL   | MIMETYPE  | STATUS  | COUNT | ");
+
         for(int i = 0; i < targetUniquesDiff.size(); i++) {
             String sUrl = targetUniquesDiff.get(i)[0];
             String sMimeType = targetUniquesDiff.get(i)[1];
@@ -400,4 +413,79 @@ public class HarUtil {
         return EntriesListRV;
 
     }
+
+    public Boolean writeHARToJSONFile(List<HarEntry> rv, String filename, String hostname){
+
+        File output_file = new File(filename);
+        ObjectMapper mapper = new ObjectMapper();
+
+
+        try (BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(output_file))) {
+
+            // ref: https://stackoverflow.com/questions/29018190/objectmapper-append-file-json
+            try (JsonGenerator g = mapper.getFactory().createGenerator(out) ) {
+                for (int i = 0; i < rv.size(); i++) {
+
+                    URL URLobject = new URL (rv.get(i).getRequest().getUrl());
+
+                    if(hostname.contains(URLobject.getHost())){
+                        //logger.debug("SKIP! "+URLobject.toString());
+                        continue;
+                    }
+
+                    Map<String, List<String>> urlMap = splitQuery(URLobject);
+
+                    String MIMETYPE = getContentType(rv.get(i).getResponse().getHeaders());
+                    urlMap.put("MIMETYPE", new LinkedList<String>());
+                    urlMap.get("MIMETYPE").add(MIMETYPE);
+
+                    String STATUS = String.valueOf(rv.get(i).getResponse().getStatus());
+                    urlMap.put("STATUS", new LinkedList<String>());
+                    urlMap.get("STATUS").add(STATUS);
+
+                    List<HarHeader> headers = rv.get(i).getRequest().getHeaders();
+                    for(int j=0; j< headers.size(); j++){
+                        if(headers.get(j).getName().contains("referer")){
+                            urlMap.put("REFERER", new LinkedList<String>());
+                            urlMap.get("REFERER").add(headers.get(j).getValue());
+                        }
+                    }
+
+                    mapper.writeValue(g, urlMap);
+                    out.write(System.getProperty("line.separator").getBytes());
+
+                }
+            }
+
+
+        }catch(IOException e){
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+    // copied from the internets
+    private static Map<String, List<String>> splitQuery(URL url) throws UnsupportedEncodingException {
+        final Map<String, List<String>> query_pairs = new LinkedHashMap<String, List<String>>();
+
+        query_pairs.put("url", new LinkedList<String>());
+        query_pairs.get("url").add(url.toString());
+
+        if(url.getQuery() != null){
+            final String[] pairs = url.getQuery().split("&");
+            for (String pair : pairs) {
+                final int idx = pair.indexOf("=");
+                final String key = idx > 0 ? URLDecoder.decode(pair.substring(0, idx), "UTF-8") : pair;
+                if (!query_pairs.containsKey(key)) {
+                    query_pairs.put(key, new LinkedList<String>());
+                }
+                final String value = idx > 0 && pair.length() > idx + 1 ? URLDecoder.decode(pair.substring(idx + 1), "UTF-8") : null;
+                query_pairs.get(key).add(value);
+            }
+        }
+
+        return query_pairs;
+    }
+
 }
